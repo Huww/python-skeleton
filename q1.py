8404f6a4cb8f0f867232954055c389e8f80efc76
Huww <huw.williams051@gmail.com>
2018-10-15 22:59:51 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -5,6 +5,6 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  answer = 0
-  print('hello')
+  answer = 15
+
   return answer
