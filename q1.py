8f450465773eaf54d9efc334574579f45fd78e2e
Huww <huw.williams051@gmail.com>
2018-10-15 23:44:40 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -5,6 +5,10 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  answer = 15
+  for p1 in portfolios:
+        for p2 in portfolios:
+            total = p1 ^ p2
+            if total > answer:
+                answer = total
 
   return answer
